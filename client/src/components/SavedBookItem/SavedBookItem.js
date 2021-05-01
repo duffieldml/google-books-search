633938ac531523204c85f75dd3adf3903f6be990
Matthew Duffield <duffieldml@gmail.com>
2021-05-01 19:13:34 -0400

    Added in savedbooks component

---
+++ client/src/components/SavedBookItem/SavedBookItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SavedBookItem(props) {
+
+    const {title, authors, description, image, link, deleteSavedBook} = props;
+    return (
+        <div>
+            <div className="card">
+                <div className="card-header"></div>
+            </div>
+        </div>
+    )
+}
