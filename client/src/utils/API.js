81f3684a4f9b3603aa384602d7f5d62f0802c05d
Matthew Duffield <duffieldml@gmail.com>
2021-05-02 14:18:37 -0400

    Updated API function call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  apiBooks: function (query) {
+  googleBooks: function (query) {
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
   },
 
